5ad2b610b1a9bf522288e025ce0950ebc08997fb
strykdevelop <strykdevelop@outlook.com>
2023-03-13 17:49:06 +0100

    initial commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,8 @@
 let mix = require('laravel-mix');
-
+const tailwindcss =require('tailwindcss')
 mix.js('src/js/app.js', 'assets')
-   .sass('src/scss/app.scss', 'assets');
+   .sass('src/scss/app.scss', 'assets')
+   .options({
+   processCssUrls: false,
+   postCss: [ tailwindcss('tailwind.config.js') ], 
+   });
